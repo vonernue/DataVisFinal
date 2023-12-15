87a23da697db780349cdc07600f24014f22b160d
Vonernue <seanho12345@gmail.com>
2023-12-15 15:15:44 +0800

    first commit

---
+++ graphs/bar.js
@@ -0,0 +1,85 @@
+export default function(data, svg, barWidth, barHeight){
+	// ------Bar Chart------
+    // Title
+    svg.append("text")
+    .attr("x", barWidth/2)
+    .attr("y", -20)
+    .attr("font-size", "14px")
+        .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .text("Hit Song by Released Month")
+
+    // X label
+    svg.append("text")
+    .attr("x", barWidth/2)
+    .attr("y", barHeight+30)
+    .attr("font-size", "14px")
+    .attr("text-anchor", "middle")
+    .text("Month")
+
+    // Y label
+    svg.append("text")
+    .attr("x", -35)
+    .attr("y", barHeight/2)
+    .attr("font-size", "14px")
+    .attr("text-anchor", "middle")
+        .attr("transform", `rotate(-90, -35, ${barHeight/2})`)
+    .text("Count")
+
+    // X axis
+    var x = d3.scaleLinear()
+        .domain([1, 13])
+        .range([0, barWidth]);
+
+    svg.append("g")
+        .attr("transform", "translate(0, " + barHeight + ")")
+        .call(d3.axisBottom(x)
+                .tickValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+            );
+
+    // Histogram
+    var hist = d3.histogram()
+    .value(function(d) {return d.released_month; })
+    .domain(x.domain())
+    .thresholds(x.ticks(12))
+
+    var bins = hist(data);
+    console.log(bins)
+    // Y axis
+    var y = d3.scaleLinear()
+            .range([barHeight, 0]);
+    y.domain([0, d3.max(bins, function(d) { return d.length; })]);   
+    svg.append("g").call(d3.axisLeft(y));
+
+    // Bars
+    svg.selectAll("barOutline")
+    .data(bins)
+    .enter()
+    .append("rect")
+        .attr("x", 0)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
+        .attr("height", function(d) { d['height']=barHeight - y(d.length); return d.height; })
+        .style("fill", "#00b7af")
+        .style("outline", "1px solid gray")
+        .classed("barOutline", true)
+
+    svg.selectAll("barRect")
+    .data(bins)
+    .enter()
+    .append("rect")
+        .attr("x", 0)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
+        .style("fill", "#fff")
+        .classed("barRect", true)
+        .transition()
+        .duration(1000)
+        .attrTween("height", function(d) {
+        var i = d3.interpolate(barHeight - y(d.length), 0);
+        return function(t) {
+            return i(t);
+        };
+        });
+}
---
+++ main.js
@@ -0,0 +1,113 @@
+import plotBar from './graphs/bar.js'
+
+const scatterLeft = 0, scatterTop = 220;
+const scatterTotalWidth = 600, scatterTotalHeight = 400;
+const scatterLegendWidth = 100, scatterLegendHeight = 400;
+const scatterMargin = {top: 40, right: 30, bottom: 40, left: 100},
+      scatterWidth = scatterTotalWidth - scatterMargin.left - scatterMargin.right - scatterLegendWidth,
+      scatterHeight = scatterTotalHeight - scatterMargin.top - scatterMargin.bottom;
+
+const donutLeft = 100, donutTop = -195;
+const donutTotalWidth = 360, donutTotalHeight = 200;
+const donutMargin = {top: 30, right: 30, bottom: 40, left: 40},
+      donutWidth = donutTotalWidth - donutMargin.left - donutMargin.right,
+      donutHeight = donutTotalHeight - donutMargin.top - donutMargin.bottom;
+
+const barLeft = 0, barTop = -400;
+const barTotalWidth = 700, barTotalHeight = 200;
+const barMargin = {top: 30, right: 30, bottom: 40, left: 100},
+      barWidth = barTotalWidth - barMargin.left - barMargin.right,
+      barHeight = barTotalHeight - barMargin.top - barMargin.bottom;
+
+const mapLeft = -100, mapTop = -200;
+const mapTotalWidth = 550, mapTotalHeight = 400;
+const mapMargin = {top: 10, right: 10, bottom: 10, left: 10},
+      mapWidth = mapTotalWidth - mapMargin.left - mapMargin.right,
+      mapHeight = mapTotalHeight - mapMargin.top - mapMargin.bottom;
+
+const scatterSvg = d3.select("#chart-area").append("svg")
+.attr("width", scatterTotalWidth)
+.attr("height", scatterTotalHeight)
+.attr("transform", `translate(${scatterLeft}, ${scatterTop})`)
+
+const scatterChart = scatterSvg.append("g")
+.attr("transform", `translate(${scatterMargin.left}, ${scatterMargin.top})`)
+
+const donutSvg = d3.select("#chart-area").append("svg")
+.attr("width", donutTotalWidth)
+.attr("height", donutTotalHeight)
+.attr("transform", `translate(${donutLeft}, ${donutTop})`)
+
+const donutChart = donutSvg.append("g")
+.attr("transform", `translate(${donutMargin.left}, ${donutMargin.top})`)
+
+const donutColor = d3.scaleOrdinal()
+.range(["#115f9a", "#008dbe", "#00b7af", "#00da72", "#d0ee11"])
+
+const donutRadius = 70
+var donutSelectedLegend = []
+
+var selectedBarDown = 0
+var selectedBarUp = 0
+var selectedBar = false
+var selectedData = []
+
+const barSvg = d3.select("#chart-area").append("svg")
+.attr("width", barTotalWidth)
+.attr("height", barTotalHeight)
+.attr("transform", `translate(${barLeft}, ${barTop})`)
+.on("mousedown", function(e) {
+  selectedBarDown = e.offsetX
+})
+.on("mouseup", function(e) {
+  selectedBarUp = e.offsetX
+  if (selectedBarDown > selectedBarUp){
+    tmp = selectedBarDown
+    selectedBarDown = selectedBarUp
+    selectedBarUp = tmp
+  }
+  barChart.append("rect")
+  .attr("x", selectedBarDown-100)
+  .attr("y", 0)
+  .attr("width", selectedBarUp - selectedBarDown)
+  .attr("height", barHeight)
+  .style("fill", "#808080")
+  .style("opacity", .3)
+  .classed("selectedBarRect", true)
+
+  barChart.selectAll(".barRect")
+  .filter(function(d) {
+    if (d == undefined) return false
+    return d.x0 * 5.7 + 100 <= selectedBarDown || d.x1 * 5.7 + 100 >= selectedBarUp
+  })
+  .transition()
+  .duration(1000)
+  .attr("height", function(d) { return d.height; }) 
+  
+  barSelectedAgeMin = Math.floor((selectedBarDown - 100) / 5.7)
+  barSelectedAgeMax = Math.floor((selectedBarUp - 100) / 5.7)
+})
+.on("click", function(e) {
+  if (!selectedBar){
+    selectedBar = true
+  }else{
+    barChart.selectAll(".selectedBarRect").remove()
+    barChart.selectAll(".barRect")
+    .transition()
+    .duration(1000)
+    .attr("height", 0)
+    barSelectedAgeMin = 0
+    barSelectedAgeMax = 100
+    selectedBar = false
+  }
+})
+
+.classed("barChart", true)
+
+const barChart = barSvg.append("g")
+.attr("transform", `translate(${barMargin.left}, ${barMargin.top})`)
+
+d3.csv("./spotify-2023.csv").then(function(data) {
+  data = data.filter((v, i, a) => v.released_year == 2023 || v.released_year == 2022)
+  plotBar(data, barChart, barWidth, barHeight)
+});
