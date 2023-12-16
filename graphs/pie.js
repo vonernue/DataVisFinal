4b034ce99ca67c03cdc543a8c41bf0041c6e5bd5
Bubublu <ntnu.huaish@gmail.com>
2023-12-17 02:48:40 +0800

    feat: add BMP radius pie chart

---
+++ graphs/pie.js
@@ -0,0 +1,210 @@
+
+export default function(data, svg, pieWidth, pieHeight){
+    // ------Radius Pie Chart------
+    var bmpData = data.map((d) => d.bpm)
+    var bins = d3.range(65, 200, 20)
+    var histogram = d3.histogram()
+        .domain([65, 200])
+        .thresholds(bins)
+    var binsData = histogram(bmpData)
+
+    // Title
+    svg.append("text")
+        .attr("x", pieWidth / 2)
+        .attr("y", -20)
+        .attr("font-size", "14px")
+        .attr("font-weight", "bold")
+        .attr("text-anchor", "middle")
+        .text("BPM Distribution")
+
+    var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(function (d) {
+            return d.data.length * 0.8;
+        });
+
+    var outerArc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(function (d) {
+            return (d.data.length + 180);
+        });
+
+    var pie = d3.pie()
+        .value(function (d) {
+            return d.length;
+        });
+
+    var colorScale = d3.scaleOrdinal()
+        .domain([0, d3.max(binsData, function (d) {
+            return d.length;
+        })])
+        .range(d3.schemeDark2);
+
+    svg.selectAll("path")
+        .data(pie(binsData))
+        .enter()
+        .append("path")
+        .attr("d", arc)
+        .attr("fill", function (d) {
+            return colorScale(d.data.length);
+        })
+        .attr("stroke", "white")
+        .style("stroke-width", "1px")
+        .attr("transform", `translate(${pieWidth / 2 - 20}, ${pieHeight / 2 + 30})`)
+        .on("mouseover", function (d) {
+            d3.select(this)
+                .style("stroke-width", "3px")
+                .style("cursor", "pointer");
+        })
+        .on("mouseout", function (d) {
+            d3.select(this)
+                .style("stroke-width", "1px");
+        })
+
+    // Legend
+    var legend = svg.append("g")
+        .attr("transform", `translate(${pieWidth - 40}, ${pieHeight - 100})`);
+
+    legend.selectAll("rect")
+        .data(binsData)
+        .enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", function (d, i) {
+            return i * 20;
+        })
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", function (d) {
+            return colorScale(d.length);
+        });
+
+    legend.selectAll("text")
+        .data(binsData)
+        .enter()
+        .append("text")
+        .attr("x", 20)
+        .attr("y", function (d, i) {
+            return i * 20 + 10;
+        })
+        .text(function (d) {
+            return d.x0 + "-" + d.x1;
+        })
+        .attr("font-size", "10px")
+        .attr("alignment-baseline", "middle");
+
+    // pie group
+    var labelGroup = svg.append("g")
+    .attr("transform", `translate(${pieWidth / 2 - 20}, ${pieHeight / 2 + 30})`)
+    .attr("id", "labelGroup");
+    
+    // add the polylines between chart and labels:
+    labelGroup.selectAll('allPolylines')
+        .data(pie(binsData))
+        .enter()
+        .append('polyline')
+        .attr("stroke", "black")
+        .style("fill", "none")
+        .attr("stroke-width", 1)
+        .attr('points', function (d) {
+            var posA = arc.centroid(d) // line insertion in the slice
+            var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+            var posC = outerArc.centroid(d); // Label position = almost the same as posB
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+            posC[0] = 1.1 * pieWidth / 3 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+            return [posA, posB, posC]
+        })
+
+    labelGroup.selectAll('allLabels')
+        .data(pie(binsData))
+        .enter()
+        .append('text')
+        .text(function (d) {
+            return d.data.length;
+        })
+        .attr("transform", function (d) {
+            var pos = outerArc.centroid(d);
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            pos[0] = pieWidth / 2.5 * (midangle < Math.PI ? 1 : -1);
+            return 'translate(' + pos + ')';
+        })
+        .style('text-anchor', function (d) {
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            return (midangle < Math.PI ? 'start' : 'end')
+        })
+        .attr("font-size", "10px")
+        .attr("alignment-baseline", "middle")
+        
+}
+
+function plotPieUpdate(data, svg, pieWidth, pieHeight, pieColor){
+    var bmpData = data.map((d) => d.bpm)
+    var bins = d3.range(65, 200, 20)
+    var histogram = d3.histogram()
+        .domain([65, 200])
+        .thresholds(bins)
+    var binsData = histogram(bmpData)
+
+
+    var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(function (d) {
+            return d.data.length * 0.8;
+        });
+    
+    var outerArc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(function (d) {
+            return (d.data.length + 180);
+        }
+    );
+
+    var pie = d3.pie()
+        .value(function (d) {
+            return d.length;
+        });
+
+    svg.selectAll("path")
+        .data(pie(binsData))
+        .transition()
+        .duration(1000)
+        .attrTween("d", function(d) {
+            var i = d3.interpolate(this._current, d);
+            this._current = i(0);
+            return function(t) {
+              return arc(i(t));
+            };
+        }
+    )
+    
+    var labelGroup = svg.select("#labelGroup")
+    labelGroup.selectAll("polyline")
+        .data(pie(binsData))
+        .transition()
+        .duration(1000)
+        .attr("points", function (d) {
+            var posA = arc.centroid(d) // line insertion in the slice
+            var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+            var posC = outerArc.centroid(d); // Label position = almost the same as posB
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+            posC[0] = 1.1 * pieWidth / 3 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+            return [posA, posB, posC]
+        })
+
+    labelGroup.selectAll("text")
+        .data(pie(binsData))
+        .transition()
+        .duration(1000)
+        .text(function (d) {
+            return d.data.length;
+        })
+        .attr("transform", function (d) {
+            var pos = outerArc.centroid(d);
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            pos[0] = pieWidth / 2.5 * (midangle < Math.PI ? 1 : -1);
+            return 'translate(' + pos + ')';
+        })
+
+}
+
+export {plotPieUpdate}
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import plotBar, {plotBarUpdate, plotBarX} from './graphs/bar.js'
+import plotPie, {plotPieUpdate} from './graphs/pie.js'
 
 var originalData = []
 var nowFilter = {sMonth: 1, eMonth: 12, sBpm: 0, eBpm: 1000}
@@ -10,39 +11,39 @@ const scatterMargin = {top: 40, right: 30, bottom: 40, left: 100},
       scatterWidth = scatterTotalWidth - scatterMargin.left - scatterMargin.right - scatterLegendWidth,
       scatterHeight = scatterTotalHeight - scatterMargin.top - scatterMargin.bottom;
 
-const donutLeft = 100, donutTop = -195;
-const donutTotalWidth = 360, donutTotalHeight = 200;
-const donutMargin = {top: 30, right: 30, bottom: 40, left: 40},
-      donutWidth = donutTotalWidth - donutMargin.left - donutMargin.right,
-      donutHeight = donutTotalHeight - donutMargin.top - donutMargin.bottom;
+const pieLeft = 100, pieTop = 0;
+const pieTotalWidth = 400, pieTotalHeight = 300;
+const pieMargin = {top: 30, right: 30, bottom: 40, left: 30},
+      pieWidth = pieTotalWidth - pieMargin.left - pieMargin.right,
+      pieHeight = pieTotalHeight - pieMargin.top - pieMargin.bottom;
 
-const barLeft = 0, barTop = -400;
-const barTotalWidth = 700, barTotalHeight = 200;
+const barLeft = 100, barTop = 0;
+const barTotalWidth = 700, barTotalHeight = 300;
 const barMargin = {top: 30, right: 30, bottom: 40, left: 100},
       barWidth = barTotalWidth - barMargin.left - barMargin.right,
       barHeight = barTotalHeight - barMargin.top - barMargin.bottom;
 
 // scatter plot
 
-const scatterSvg = d3.select("#chart-area").append("svg")
-.attr("width", scatterTotalWidth)
-.attr("height", scatterTotalHeight)
-.attr("transform", `translate(${scatterLeft}, ${scatterTop})`)
+// const scatterSvg = d3.select("#chart-area").append("svg")
+// .attr("width", scatterTotalWidth)
+// .attr("height", scatterTotalHeight)
+// .attr("transform", `translate(${scatterLeft}, ${scatterTop})`)
 
-const scatterChart = scatterSvg.append("g")
-.attr("transform", `translate(${scatterMargin.left}, ${scatterMargin.top})`)
+// const scatterChart = scatterSvg.append("g")
+// .attr("transform", `translate(${scatterMargin.left}, ${scatterMargin.top})`)
 
 // donut chart
 
 const donutSvg = d3.select("#chart-area").append("svg")
-.attr("width", donutTotalWidth)
-.attr("height", donutTotalHeight)
-.attr("transform", `translate(${donutLeft}, ${donutTop})`)
+.attr("width", pieTotalWidth)
+.attr("height", pieTotalHeight)
+.attr("transform", `translate(${pieLeft}, ${pieTop})`)
 
 const donutChart = donutSvg.append("g")
-.attr("transform", `translate(${donutMargin.left}, ${donutMargin.top})`)
+.attr("transform", `translate(${pieMargin.left}, ${pieMargin.top})`)
 
-const donutColor = d3.scaleOrdinal()
+const pieColor = d3.scaleOrdinal()
 .range(["#115f9a", "#008dbe", "#00b7af", "#00da72", "#d0ee11"])
 
 const donutRadius = 70
@@ -74,6 +75,7 @@ d3.csv("./spotify-2023.csv").then(function(data) {
   originalData = data.filter((v, i, a) => v.released_year == 2023 || v.released_year == 2022)
   
   plotBar(originalData, barChart, barWidth, barHeight)
+  plotPie(originalData, donutChart, pieWidth, pieHeight, pieColor)
   // This line should be drawn after the bars
   // or the brush will be covered by the bars
   barChart.append("g").call(barBrush);
@@ -118,4 +120,5 @@ function updateView(filter){
     v.bpm <= filter.eBpm
   )
   plotBarUpdate(data, barChart, barWidth, barHeight)
+  plotPieUpdate(data, donutChart, pieWidth, pieHeight)
 }
